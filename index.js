69e683f2f74653f1c1009b190922ff934f68bf40
yash patel <pyash2819@gmail.com>
2024-06-30 13:56:05 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const PORT = 3000;
+
+app.get('/', (req,res) => {
+    res.send('Hello!!!!!!!!');
+});
+
+app.listen(PORT,() => {
+    console.log(`http://localhost:${PORT}`);
+});
